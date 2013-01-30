83d886ed5f73dcbba11a275645d941bdd6f7c61a
Kent Nguyen <nguyen.dmz@gmail.com>
2013-01-31 03:07:13 +0800

    Add more customisation constants

--- Vurig Calendar/VRGCalendarView.h
+++ Vurig Calendar/VRGCalendarView.h
@@ -6,44 +6,54 @@
 //  Copyright (c) 2012 Vurig Media. All rights reserved.
 //
 
-
 #import <UIKit/UIKit.h>
 #import "UIColor+expanded.h"
 
-#define kVRGCalendarViewTopBarHeight 60
-#define kVRGCalendarViewWidth 320
+#define kVRGCalendarViewTopBarHeight    70
+#define kVRGCalendarViewTitleHeight     50
+#define kVRGCalendarViewWidth          320
 
-#define kVRGCalendarViewDayWidth 44
-#define kVRGCalendarViewDayHeight 44
+#define kVRGCalendarViewDayWidth        44
+#define kVRGCalendarViewDayHeight       44
 
-#define kVRGCalendarTextColor @"0x383838"
-#define kVRGCalendarArrowColor @"0x000000"
-#define kVRGCalendarBackgroundColor @"0xFFFFFF"
-#define kVRGCalendarPrevOrNextTextColor @"0x767676"
-#define kVRGCalendarSelectedTextBackgroundColor @"0x006dbc"
-#define kVRGCalendarSelectedTextColor @"0xFFFFFF"
-#define kVRGCalendarDisableTextColor @"0xDCDCDC"
-#define kVRGCalendarGridBackgroundColor @"0xf3f3f3"
-#define kVRGCalendarGridDarkLinesColor @"0xcfd4d8"
+#define kVRGCalendarTextColor                   @"0x000000"
+#define kVRGCalendarArrowColor                  @"0x000000"
+#define kVRGCalendarBackgroundColor             @"0xFFFFFF"
+#define kVRGCalendarPrevOrNextTextColor         @"0x767676"
+#define kVRGCalendarSelectedTextBackgroundColor @"0xFF8000"
+#define kVRGCalendarSelectedTextColor           @"0xFFFFFF"
+#define kVRGCalendarTodayBackgroundColor        @"0xEEEEEE"
+#define kVRGCalendarTodayTextColor              @"0x383838"
+#define kVRGCalendarDisableTextColor            @"0xDCDCDC"
+#define kVRGCalendarGridBackgroundColor         @"0xFFFFFF"
+#define kVRGCalendarGridDarkLinesColor          @"0xFFFFFF"
+#define kVRGCalendarDayLabelColor               @"0x919191"
+#define kVRGCalendarTitleLabelColor             @"0x5E5E5E"
 
+#define kKNCalendarTitleFont          [UIFont fontWithName:@"HelveticaNeue" size:22]
+#define kKNCalendarNumberFont         [UIFont fontWithName:@"HelveticaNeue" size:22]
+#define kKNCalendarDayLabelFont       [UIFont fontWithName:@"HelveticaNeue-Bold" size:13]
+
+#define kKNCalendarArrowHeight        18
+#define kKNCalendarArrowOffsetTop     14
+#define kKNCalendarArrowOffsetSide    18
 
 @protocol VRGCalendarViewDelegate;
 @interface VRGCalendarView : UIView {
-//    id <VRGCalendarViewDelegate> delegate;
-    
+
     NSDate *currentMonth;
     NSDate *beginDate;
     NSDate *endDate;
-    
+
     UILabel *labelCurrentMonth;
-    
+
     BOOL isAnimating;
     BOOL prepAnimationPreviousMonth;
     BOOL prepAnimationNextMonth;
-    
+
     UIImageView *animationView_A;
     UIImageView *animationView_B;
-    
+
     NSArray *markedDates;
     NSArray *markedColors;
 }
